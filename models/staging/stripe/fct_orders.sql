24ae793de66b6a11b2251cb3ed948b42b0e21bc5
Huizhe ZHU <hz2657@columbia.edu>
2020-12-27 02:24:08 +0000

    commit

--- models/staging/stripe/fct_orders.sql
+++ models/staging/stripe/fct_orders.sql
@@ -1,10 +1,18 @@
-with orders as (
-    select * from {{ ref('stg_orders')}}
+with orders as  (
+    select * from {{ ref('stg_orders' )}}
 ),
 
 payments as (
     select * from {{ ref('stg_payments') }}
-    WHERE status = 'success'
+),
+
+order_payments as (
+    select
+        order_id,
+        sum(case when status = 'success' then amount end) as amount
+
+    from payments
+    group by 1
 ),
 
 final as (
@@ -12,8 +20,11 @@ final as (
     select
         orders.order_id,
         orders.customer_id,
-        payments.amount
+        orders.order_date,
+        coalesce(order_payments.amount, 0) as amount
 
-    from orders LEFT JOIN payments using(order_id)
+    from orders
+    left join order_payments using (order_id)
 )
+
 select * from final
